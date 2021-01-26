476c157947a82de221e6887d258c6d6bf8c93037
LUCAS FERREIRA NOLETO <LFNOLETO@jmnetid.com>
2021-01-26 08:09:58 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link'
+
 function Home (){
-    return <div>Home</div>
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link>
+                <a href= "/sobre">acessar página sobre</a>
+            </Link>
+        </div>)
 }
 
 export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+function Sobre (){
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <Link>
+                <a href='/'>acessar página Home</a>
+            </Link>
+        </div>)
+}
+
+export default Sobre;
